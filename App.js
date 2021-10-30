6580c698442bef12faf203e93713e135d428ac73
Herbie09 <90429660+Herbie09@users.noreply.github.com>
2021-10-31 00:58:35 +0100

    Update App.js

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Login from './Screens/Login';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Shopping App</Text>
-      <StatusBar style="auto" />
+   <View style={styles.container}>
+      <StatusBar 
+        backgroundColor='grey'
+        barStyle= 'light-content'
+      />
+      <Login/>
     </View>
   );
 }
