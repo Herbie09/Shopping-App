20d4c2a6530dbe848847f6f56ea41419505e425f
Herbie09 <90429660+Herbie09@users.noreply.github.com>
2021-11-03 01:03:27 +0000

    Add files via upload

---
+++ SignUp.js
@@ -0,0 +1,71 @@
+import React from "react";
+import {View, TextInput, StyleSheet, Text, TouchableOpacity, Image, Button} from "react-native";
+
+export default function App (){
+    return(
+        <View style={styles.sharp}>
+         <Text style={styles.hka}>Herbie's Shopping App</Text>   
+               <TextInput style= {styles.inputbox}
+                    placeholder="First Name"
+                    placeholderTextColor={'grey'}/>
+
+               <TextInput style= {styles.inputbox}
+                    placeholder="Last Name"
+                    placeholderTextColor={'grey'}
+                   />
+                <TextInput style= {styles.inputbox}
+                    placeholder="Email"
+                    placeholderTextColor={'grey'}
+                   />
+                <TextInput style= {styles.inputbox}
+                    placeholder="Contact"
+                    placeholderTextColor={'grey'}
+                   />
+          <TouchableOpacity>
+             <Text style={styles.signuppart}> Sign Up</Text>
+          </TouchableOpacity>
+
+            <View style={styles.signintext}>
+                 <Text style={{color:'#fff'}}>Do you have an account?</Text>
+                 <Text style={{fontWeight:'800', fontSize:16}}>Sign In</Text>
+            </View>
+        </View>
+    );   
+}
+
+const styles = StyleSheet.create({
+    sharp: {
+      flex: 1,
+      backgroundColor:'purple',
+      alignItems: 'center',
+      // justifyContent: 'center',
+     },
+    inputbox: {
+      width: 230,
+      height: 30,
+      backgroundColor: 'white',
+      borderColor: 'black',
+      borderRadius: 20 ,
+      paddingLeft: 12,
+      marginVertical: 8,
+    },
+    signuppart:{
+      fontSize: 16,
+      fontWeight:'bold',
+      borderRadius: 20 ,
+      width:150,
+      backgroundColor:'rgba(255,255,255,0.3)',
+      textAlign:'center',
+      marginVertical:16,
+    },
+    signintext:{
+      paddingTop: 40,
+      flexDirection: 'row',
+    },
+    hka:{
+      paddingTop:80,
+      paddingBottom:180, 
+      fontSize:30,
+      fontFamily:'sans-serif',
+    },
+  });
