bbeda676f91516a2221d5439644766ffabd3454c
Herbie09 <90429660+Herbie09@users.noreply.github.com>
2021-11-11 01:23:37 +0000

    Update App.js

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-import Login from './Screens/Login';
-
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 export default function App() {
   return (
