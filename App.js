287ae00cb54aca871e46911a7e48142e4bbb4aa4
Herbie09 <90429660+Herbie09@users.noreply.github.com>
2021-11-14 22:05:12 +0000

    Update App.js

--- App.js
+++ App.js
@@ -1,27 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React from "react";
+import { StyleSheet, View, Button}from "react-native";
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Login from './Screens/Login';
 import SignUp from './Screens/SignUp';
 
-export default function App() {
-  const Stack = createStackNavigator();
 
+  const Stack = createStackNavigator();
+  export default function App() {
   return (
-   <View style={styles.container}>
-      <StatusBar 
+    <View style= {{flex:1}}>
+      {/* <StatusBar 
         backgroundColor='grey'
         barStyle= 'light-content'
-      />
-       <NavigationContainer>
-         <MainNavigator.Navigator screenOptions={{ headerShown: true ,  }}>
-
-           <MainNavigator.Screen name="Login" component={Login} />
-           <MainNavigator.Screen name="SignUp" component={SignUp} />
-         </MainNavigator.Navigator>
-       </NavigationContainer>
+      /> */}
+      {/* <Login/> */}
+      {/* <SignUp/> */}
+      
+      <NavigationContainer>
+         <Stack.Navigator screenOptions={{ headerShown: true ,  }}>
+             <Stack.Screen name="Login" component={Login} />
+             <Stack.Screen name="SignUp" component={SignUp}/>
+         </Stack.Navigator>
+      </NavigationContainer>
+     
+       {/* <Stack.Screen
+           name="Login"
+           component={Login}
+           options={{ title: 'Overview' }}
+        /> */}
     </View>
   );
 }
