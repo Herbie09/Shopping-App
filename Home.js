e291ee964dfe3b9ebde27d0b1ee93391e43795f3
Herbie09 <90429660+Herbie09@users.noreply.github.com>
2021-11-14 21:59:21 +0000

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function Home(params) {
+    const navigation = params.navigation;
+    return (
+        <View>
+            <Text>Welcome</Text>
+        </View>
+    )
+}
+
