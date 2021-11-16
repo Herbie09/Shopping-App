e41e39744d1e1e5ae617ae321837f60aac1a47e2
Herbie09 <90429660+Herbie09@users.noreply.github.com>
2021-11-16 00:40:54 +0000

    Update App.js

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Login from './Screens/Login';
 import SignUp from './Screens/SignUp';
+import Home from './Screens/Home';
+import Cart from './Screens/Cart';
 
 
   const Stack = createStackNavigator();
@@ -12,18 +14,20 @@ import SignUp from './Screens/SignUp';
   return (
     <View style= {{flex:1}}>
       {/* <StatusBar 
-        backgroundColor='grey'
+        backgroundColor=''
         barStyle= 'light-content'
       /> */}
       {/* <Login/> */}
       {/* <SignUp/> */}
       
-      <NavigationContainer>
-         <Stack.Navigator screenOptions={{ headerShown: true ,  }}>
-             <Stack.Screen name="Login" component={Login} />
-             <Stack.Screen name="SignUp" component={SignUp}/>
-         </Stack.Navigator>
-      </NavigationContainer>
+          <NavigationContainer>
+            <Stack.Navigator screenOptions={{ headerShown: true, }}>
+                 <Stack.Screen name="Login" component={Login} />
+                 <Stack.Screen name="SignUp" component={SignUp}/>
+                 <Stack.Screen name="Home" component={Home}/>
+                 <Stack.Screen name="Cart" component={Cart}/>
+            </Stack.Navigator>
+          </NavigationContainer>
      
        {/* <Stack.Screen
            name="Login"
@@ -41,4 +45,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+ });
